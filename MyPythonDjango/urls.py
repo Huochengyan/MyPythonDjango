48015b13a0c43594e8935d315bc93095578e04d1
Administrator <Administrator@PC201703292302>
2018-06-21 00:08:08 +0800

    创建Djan项目myweb 添加index View

--- MyPythonDjango/urls.py
+++ MyPythonDjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+#1. 导入视图层
+from myweb import  views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.index),
 ]
